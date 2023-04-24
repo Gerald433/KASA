8b363e08c959e6ed41c2ef0ad66eef7e972f7e1e
Gérald Gaillard <gerald8142@gmail.com>
2023-04-24 21:47:13 +0200

    End the Error page

--- src/pages/Error/Error.js
+++ src/pages/Error/Error.js
@@ -4,17 +4,17 @@ import { Link } from "react-router-dom";
 
 const Error = function () {
   return (
-    <div>
-      <section className={styles.section}>
-        <h1 className={styles.h1}>404</h1>
-        <h2 className={styles.h2}>Oups! La page que vous demandez n'existe pas.</h2>
-        <h3 className={styles.h3}>
+    <section className={styles.section}>
+      <h1 className={styles.h1}>404</h1>
+      <h2 className={styles.h2}>
+        Oups! La page que vous demandez n'existe pas.
+      </h2>
+      <h3 className={styles.h3}>
         <Link to="/" className={styles.link}>
-        Retourner sur la page d'accueil
-          </Link>
-        </h3>
-      </section>
-    </div>
+          Retourner sur la page d'accueil
+        </Link>
+      </h3>
+    </section>
   );
 };
 
