e00a87cd8584d199cf841d43cb5d769bcebb4ae9
Gérald Gaillard <gerald8142@gmail.com>
2023-04-24 22:13:41 +0200

    Create the Apropos page

---
+++ src/pages/Apropos/Apropos.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Banner from "../../components/Banner/Banner";
+import Accordeon from "../../components/Accordeon/Accordeon";
+
+const Apropos = function () {
+  return (
+    <main>
+      <Banner />
+      {/* <Accordeon /> */}
+    </main>
+      
+  );
+};
+
+export default Apropos;
