a6f858cc2c67c9be002d96a527e4206b316e9cf9
Gérald Gaillard <gerald8142@gmail.com>
2023-04-24 11:20:32 +0200

    import the header

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import generalCss from "./assets/css/general.module.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    {/* <Header />
-    <Routes>
+    <Header />
+    {/* <Routes>
       <Route path="*" element={<Error />}/>
       <Route path="/" element={<Home />}/>
       <Route path="/card/:cardId" element={<Card />}/>
