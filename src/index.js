f12fd30dd2f0d372a33dbf6d57c01f1a68aa96b9
Gérald Gaillard <gerald8142@gmail.com>
2023-04-24 22:17:16 +0200

    uncomment route a propos and import

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Error from "./pages/Error/Error";
+import Apropos from "./pages/Apropos/Apropos";
 import resetCss from "./assets/css/reset.module.css";
 import generalCss from "./assets/css/general.module.css";
 
@@ -15,10 +16,10 @@ root.render(
   <BrowserRouter>
     <Header />
     <Routes>
-      <Route path="*" element={<Error />}/>
-      {/* <Route path="/" element={<Home />}/>
-      <Route path="/card/:cardId" element={<Card />}/>
-      <Route path="/a-propos" element={<Apropos />}/> */}
+      {/* <Route path="*" element={<Error />}/>
+      <Route path="/" element={<Home />}/>
+      <Route path="/card/:cardId" element={<Card />}/> */}
+      <Route path="/a-propos" element={<Apropos />}/>
     </Routes>
     <Footer />
   </BrowserRouter>
