579340460c5feea841b4ffbbfe4b1296e6d873fa
Gérald Gaillard <gerald8142@gmail.com>
2023-04-24 16:37:16 +0200

    Import Footer component resetCss and generalCss

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import resetCss from "./assets/css/reset.module.css";
 import generalCss from "./assets/css/general.module.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -14,8 +16,8 @@ root.render(
       <Route path="/" element={<Home />}/>
       <Route path="/card/:cardId" element={<Card />}/>
       <Route path="/a-propos" element={<Apropos />}/>
-    </Routes>
-    <Footer /> */}
+    </Routes> */}
+    {/* <Footer /> */}
   </BrowserRouter>
 );
 
