a88d8884231ec4f82f596c5a52c7c4685c0e0eae
Gérald Gaillard <gerald8142@gmail.com>
2023-04-24 18:59:43 +0200

    Create Error page

---
+++ src/pages/Error/Error.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./Error.module.css";
+import { Link } from "react-router-dom";
+
+const Error = function () {
+  return (
+    <div>
+      <section className={styles.section}>
+        <h1 className={styles.h1}>404</h1>
+        <h2 className={styles.h2}>Oups! La page que vous demandez n'existe pas.</h2>
+        <h3 className={styles.h3}>
+        <Link to="/" className={styles.link}>
+        Retourner sur la page d'accueil
+          </Link>
+        </h3>
+      </section>
+    </div>
+  );
+};
+
+export default Error;
