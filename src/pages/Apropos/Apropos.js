b2a323a8b1d9cb604595f55b6a5e8c63ec761dc2
Gérald Gaillard <gerald8142@gmail.com>
2023-04-25 14:00:03 +0200

    recover banner image apropos

--- src/pages/Apropos/Apropos.js
+++ src/pages/Apropos/Apropos.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Banner from "../../components/Banner/Banner";
 import Accordeon from "../../components/Accordeon/Accordeon";
+import background2 from "../../assets/images/kalen-emsley-Bkci_8qcdvQ-unsplash 2 (1).jpg";
 
 const Apropos = function () {
   return (
     <main>
-      <Banner />
+      <Banner src={background2} />
       <Accordeon />
     </main>
-      
   );
 };
 
