f976be179bad2128443d02b9b412d5eb92005e86
Gérald Gaillard <gerald8142@gmail.com>
2023-04-25 09:43:27 +0200

    end the accordeon component

--- src/components/Accordeon/Accordeon.js
+++ src/components/Accordeon/Accordeon.js
@@ -3,45 +3,43 @@ import styles from "./Accordeon.module.css";
 
 const Accordeon = function () {
   return (
-    <article className={styles.containerGlobal}>
-      <section className={styles.quality}>
-        <details>
-          <summary>Fiabilité</summary>
-          <p className={styles.accordeonText}>
-            Les annonces postées sur Kasa garantissent une fiabilité totale. Les
-            photos sont conformes aux logements, et toutes les informations sont
-            régulièrement vérifiées par nos équipes.
-          </p>
-        </details>
-        <details>
-          <summary>Respect</summary>
-          <p className={styles.accordeonText}>
-            La surveillance fait partie des valeurs fondatrices de Kasa. Tout
-            comportement discriminatoire ou de perturbation du voisinage
-            entraînera une exclusion de notre plateforme.
-          </p>
-        </details>
-        <details>
-          <summary>Service</summary>
-          <p className={styles.accordeonText}>
-            Nos équipes se tiennent à votre disposition pour vous fournir une
-            expérience parfaite. N'hésitez pas à nous contacter si vous avez la
-            moindre question.
-          </p>
-        </details>
-        <details>
-          <summary>Sécurité</summary>
-          <p className={styles.accordeonText}>
-            La sécurité est la priorité de kasa. Aussi bien pour nos hôtes que
-            pour les voyageurs, chaque logement correspond aux critères de
-            sécurité établis par nos services. En laissant une note aussi bien à
-            l'hôte qu'au locataire, cela permet à nos équipes de vérifier que
-            les standards sont bien respectés. Nous organisons également des
-            ateliers sur la sécurité domestique pour nos hôtes.
-          </p>
-        </details>
-      </section>
-    </article>
+    <section className={styles.quality}>
+      <details className={styles.details}>
+        <summary className={styles.summary}>Fiabilité</summary>
+        <p className={styles.accordeonText}>
+          Les annonces postées sur Kasa garantissent une fiabilité totale. Les
+          photos sont conformes aux logements, et toutes les informations sont
+          régulièrement vérifiées par nos équipes.
+        </p>
+      </details>
+      <details className={styles.details}>
+        <summary className={styles.summary}>Respect</summary>
+        <p className={styles.accordeonText}>
+          La surveillance fait partie des valeurs fondatrices de Kasa. Tout
+          comportement discriminatoire ou de perturbation du voisinage
+          entraînera une exclusion de notre plateforme.
+        </p>
+      </details>
+      <details className={styles.details}>
+        <summary className={styles.summary}>Service</summary>
+        <p className={styles.accordeonText}>
+          Nos équipes se tiennent à votre disposition pour vous fournir une
+          expérience parfaite. N'hésitez pas à nous contacter si vous avez la
+          moindre question.
+        </p>
+      </details>
+      <details className={styles.details}>
+        <summary className={styles.summary}>Sécurité</summary>
+        <p className={styles.accordeonText}>
+          La sécurité est la priorité de kasa. Aussi bien pour nos hôtes que
+          pour les voyageurs, chaque logement correspond aux critères de
+          sécurité établis par nos services. En laissant une note aussi bien à
+          l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les
+          standards sont bien respectés. Nous organisons également des ateliers
+          sur la sécurité domestique pour nos hôtes.
+        </p>
+      </details>
+    </section>
   );
 };
 
