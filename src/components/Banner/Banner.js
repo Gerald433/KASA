d369261a615b77abc2cf08268a16154d38fac3fc
Gérald Gaillard <gerald8142@gmail.com>
2023-04-25 13:56:49 +0200

    use of props with banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,12 +2,13 @@ import React from "react";
 import background2 from "../../assets/images/kalen-emsley-Bkci_8qcdvQ-unsplash 2 (1).jpg";
 import styles from "./Banner.module.css";
 
-const Banner = function () {
+const Banner = function (props) {
+  console.log(props)
   return (
     <section className={styles.pubSiteSection}>
       <img
         className={styles.photoPubSite}
-        src={background2}
+        src={props.src}
         alt="photo de chaine de montagne et de forets."
       />
     </section>
