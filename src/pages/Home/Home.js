f942db6e396007a8e6f0d938e616564fbd8d573b
Gérald Gaillard <gerald8142@gmail.com>
2023-04-25 15:44:40 +0200

    recenter banner title

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,30 +1,15 @@
 import React from "react";
-import styles from "./Home.module.css"
-import background from "../../assets/images/photoBackgroundIndex.jpg"
+import styles from "./Home.module.css";
+import background from "../../assets/images/photoBackgroundIndex.jpg";
 import Banner from "../../components/Banner/Banner";
 import Thumb from "../../components/Thumb/Thumb";
 
-
 const Home = function () {
   return (
-    <main>
+    <section className={styles.pubSiteSection}>
       <Banner src={background} />
-
-      <section className={styles.pubSiteSection}>
-        
-        <h1 className={styles.titlePubSite}>Chez vous, partout et ailleurs</h1>
-      </section>
-
-
-
-
-
-
-
-
-
-      
-    </main>
+      <h1 className={styles.titlePubSite}>Chez vous, partout et ailleurs</h1>
+    </section>
   );
 };
 
