39700ba4b1234b38e5eb3f337ed0b8a3394a9416
Gérald Gaillard <gerald8142@gmail.com>
2023-04-25 17:00:59 +0200

    format the home page with Thumb

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import styles from "./Home.module.css";
 import background from "../../assets/images/photoBackgroundIndex.jpg";
 import Banner from "../../components/Banner/Banner";
@@ -6,10 +6,15 @@ import Thumb from "../../components/Thumb/Thumb";
 
 const Home = function () {
   return (
-    <section className={styles.pubSiteSection}>
-      <Banner src={background} />
-      <h1 className={styles.titlePubSite}>Chez vous, partout et ailleurs</h1>
-    </section>
+    <Fragment>
+      <section className={styles.pubSiteSection}>
+        <Banner src={background} />
+        <h1 className={styles.titlePubSite}>Chez vous, partout et ailleurs</h1>
+      </section>
+      <section className={styles.cardTable}>
+        <Thumb />
+      </section>
+    </Fragment>
   );
 };
 
