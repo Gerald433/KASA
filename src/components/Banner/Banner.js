186f67f40454d090ea15344ab83edf9246844c0d
Gérald Gaillard <gerald8142@gmail.com>
2023-05-03 09:10:14 +0200

    create a modular banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,14 +1,20 @@
 import React from "react";
-import background2 from "../../assets/images/kalen-emsley-Bkci_8qcdvQ-unsplash 2 (1).jpg";
+// import background2 from "../../assets/images/kalen-emsley-Bkci_8qcdvQ-unsplash 2 (1).jpg";
 import styles from "./Banner.module.css";
 
-const Banner = function (props) {
-  console.log(props)
+const Banner = function ({ src, large }) {
+  console.log(large);
   return (
-    <section className={styles.pubSiteSection}>
+    <section
+      className={
+        large
+          ? styles.mobileLarge + " " + styles.pubSiteSection
+          : styles.pubSiteSection
+      }
+    >
       <img
         className={styles.photoPubSite}
-        src={props.src}
+        src={src}
         alt="photo de chaine de montagne et de forets."
       />
     </section>
