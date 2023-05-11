6f6963ac99ff918ad9b8f708c9a773331564156c
Gérald Gaillard <gerald8142@gmail.com>
2023-05-11 10:30:53 +0200

    collect votes with stars

--- src/components/Star/Star.js
+++ src/components/Star/Star.js
@@ -1,14 +1,16 @@
 import React from "react";
 import styles from "./Star.module.css";
-import starOff from "../../assets/images/redStar.svg";
+import starOn from "../../assets/images/redStar.svg";
+import starOff from "../../assets/images/greyStar.svg";
 
-const Star = function () {
-
+const Star = function ({cheked}) {
+  
+  
     return (
         <div className={styles.listStarOff}>
         <img
           className={styles.starOff}
-          src={starOff}
+          src={cheked? starOn : starOff }
           alt="étoile de vote non sélectionnée"
         />
       </div>
