a43bd70932214251a9f9ca0462801922ca0a2d00
Gérald Gaillard <gerald8142@gmail.com>
2023-05-14 17:41:21 +0200

    Improve implementation of Thumbs

--- src/components/Thumb/Thumb.js
+++ src/components/Thumb/Thumb.js
@@ -1,41 +1,15 @@
 import React from "react";
 import styles from "./Thumb.module.css";
-import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-const Thumb = function () {
-  const [cards, setCards] = useState([]);
-  useEffect(() => {
-    async function fetchCards() {
-      try {
-        const _cards = await (await fetch("/data/logements.json")).json();
-        setCards(_cards);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    fetchCards();
-  }, []);
-
+const Thumb = function ({ card }) {
   return (
-    <section className={styles.section}>
-      <ul className={styles.containerItems}>
-        {cards.map((card) => {
-          return (
-            <li className={styles.card} key={card.id}>
-              <Link to={`/card/${card.id}`} className={styles.linkImage}>
-                <img
-                  src={card.cover}
-                  alt={card.title}
-                  className={styles.imageCard}
-                />
-                <h2 className={styles.title}>{card.title}</h2>
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </section>
+    <li className={styles.card} key={card.id}>
+      <Link to={`/card/${card.id}`} className={styles.linkImage}>
+        <img src={card.cover} alt={card.title} className={styles.imageCard} />
+        <h2 className={styles.title}>{card.title}</h2>
+      </Link>
+    </li>
   );
 };
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,23 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import styles from "./Home.module.css";
 import background from "../../assets/images/photoBackgroundIndex.jpg";
 import Banner from "../../components/Banner/Banner";
 import Thumb from "../../components/Thumb/Thumb";
 
 const Home = function () {
+  const [cards, setCards] = useState([]);
+  useEffect(() => {
+    async function fetchCards() {
+      try {
+        const _cards = await (await fetch("/data/logements.json")).json();
+        setCards(_cards);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchCards();
+  }, []);
+
   return (
     <Fragment>
       <section className={styles.pubSiteSection}>
@@ -12,7 +25,11 @@ const Home = function () {
         <h1 className={styles.titlePubSite}>Chez vous, partout et ailleurs</h1>
       </section>
       <section className={styles.cardTable}>
-        <Thumb />
+        <ul className={styles.containerItems}>
+          {cards.map((card, index) => (
+            <Thumb card={card} key={index} />
+          ))}
+        </ul>
       </section>
     </Fragment>
   );
